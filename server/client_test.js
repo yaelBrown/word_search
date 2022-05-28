67b1af5901c869784826107901975b04fbcf8457
yaelBrown <yaelrbrown@gmail.com>
2022-05-27 19:40:50 -0500

    feat: basic client and server impl

---
+++ server/client_test.js
@@ -0,0 +1,7 @@
+
+const { io } = require("socket.io-client");
+const socket = io("http://localhost:3000/")
+
+socket.on('connect', () => {
+  console.log(`${socket.io}: connected server`)
+})
---
+++ server/index.js
@@ -0,0 +1,22 @@
+// https://www.youtube.com/watch?v=ZKEqqIO7n-k
+
+const createServer = require("http").createServer;
+const Server = require("socket.io");
+
+const httpServer = createServer();
+const io = new Server(httpServer, {})
+
+io.on("connection", socket => {
+  io.emit("A New Challenger Appeared!")
+  // Sends to all of the clients and the one that made the request at first.
+  console.log(`${socket.id}: new connection`)
+  /*
+    For reference - 
+    socket.on("send-message", message => {
+      socket.broadcast.emit("recieve message", message)
+      // sends to everyone besides the person who initiates it.  
+    })
+  */ 
+})
+
+httpServer.listen(3000)
